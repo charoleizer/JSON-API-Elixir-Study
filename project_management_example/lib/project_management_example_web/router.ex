4f617307e37048607ada711c2fdbbf6c63eaa043
Victor Zanella <victor.t.y.z@hotmail.com>
2019-06-15 02:18:38 -0300

    updates

--- project_management_example/lib/project_management_example_web/router.ex
+++ project_management_example/lib/project_management_example_web/router.ex
@@ -13,14 +13,11 @@ defmodule ProjectManagementExampleWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", ProjectManagementExampleWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
   # Other scopes may use custom stacks.
-  # scope "/api", ProjectManagementExampleWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", ProjectManagementExampleWeb do
+    pipe_through :api
+
+    resources "/projects", ProjectController, only: [:index, :show]
+    #get "/projects/:id", ProjectController, :index
+  end
 end
---
+++ project_management_example/priv/repo/seeds/project_seeds.exs
@@ -0,0 +1,17 @@
+alias ProjectManagementExample.Repo
+alias ProjectManagementExample.Management.Project
+
+Repo.insert! %Project{
+    title: "Super cool study project",
+    description: "This project is my first test with Elixir + Phoenix"
+}
+
+Repo.insert! %Project{
+    title: "Super cool study project",
+    description: "This project is my first test with Elixir + Phoenix"
+}
+
+Repo.insert! %Project{
+    title: "Super cool study project",
+    description: "This project is my first test with Elixir + Phoenix"
+}
